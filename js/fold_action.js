67876eec3df74ff5cb61faeac706bb01b3283765
David <1656409967@qq.com>
2020-06-06 19:22:50 +0800

    Site updated: 2020-06-06 19:22:15

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-    $(document).on('click', '.fold_hider', function(){
-        $('>.fold', this.parentNode).slideToggle();
-        $('>:first', this).toggleClass('open');
-    });
-    //默认情况下折叠
-    $("div.fold").css("display","none");
-});
+// build time:Sat Jun 06 2020 19:21:49 GMT+0800 (GMT+08:00)
+$(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
+//rebuild by neat 
