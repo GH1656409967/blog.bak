22c3a85c90daca429a4c75a70044a72a2e82edc4
David <1656409967@qq.com>
2020-07-26 14:06:58 +0800

    Site updated: 2020-07-26 14:05:42

--- js/fold_action.js
+++ js/fold_action.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 20 2020 14:50:06 GMT+0800 (GMT+08:00)
+// build time:Sun Jul 26 2020 14:05:31 GMT+0800 (GMT+08:00)
 $(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
 //rebuild by neat 
