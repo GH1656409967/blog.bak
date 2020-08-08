6b587e6001f9ea771effed3af5f38e6d53a4b0fe
David <1656409967@qq.com>
2020-08-08 16:12:07 +0800

    Site updated: 2020-08-08 16:10:58

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 08 2020 15:58:14 GMT+0800 (GMT+08:00)
+// build time:Sat Aug 08 2020 16:10:49 GMT+0800 (GMT+08:00)
 !function(n,t,o){var e=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+="<span>复制</span>";n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};e()}(window,document);
 //rebuild by neat 
