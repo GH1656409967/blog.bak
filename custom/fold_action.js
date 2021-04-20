016846b10388d50857be2df1505abf192aa65fe6
David <1656409967@qq.com>
2021-04-20 20:50:24 +0800

    Site updated: 2021-04-20 20:48:26

--- custom/fold_action.js
+++ custom/fold_action.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 19 2021 21:52:48 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 20 2021 20:48:02 GMT+0800 (GMT+08:00)
 $(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
 //rebuild by neat 
